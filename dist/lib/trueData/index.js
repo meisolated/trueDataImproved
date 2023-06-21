06f6269cb3960a7db5e3e177345b7124124ffe5a
meisolated <isolatedbot@gmail.com>
2023-06-21 16:57:29 +0530

    first commuit

--- dist/lib/trueData/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MarketFeeds = void 0;
-const marketFeeds_1 = __importDefault(require("./marketFeeds"));
-exports.MarketFeeds = marketFeeds_1.default;
